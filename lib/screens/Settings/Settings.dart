fc1f4faba652956f98a5b992478a2748a36e9bd6
Madusha <madusha.wicramasinghe@zligger.com>
2021-04-06 10:59:52 +0530

    settings page

--- lib/screens/Settings/Settings.dart
+++ lib/screens/Settings/Settings.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:jennings/screens/Animation/FadeAnimation.dart';
+import 'package:jennings/screens/Home.dart';
 
 class Settings extends StatefulWidget {
   @override
@@ -9,9 +11,185 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 1,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.blue,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => Home()));
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.settings,
+              color: Colors.blue,
+            ),
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) => Settings()));
+            },
+          ),
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Center(
+                  child: Text(
+                    "Settings",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
 
+
+              Positioned(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 150),
+                    child: Column(
+                        children:<Widget> [
+                          FadeAnimation(2,
+                            Container(
+                                padding: EdgeInsets.all(8.0),
+                                decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(color: Colors.grey[100]))
+                                ),
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "Theme",style: TextStyle(color: Colors.grey[400],fontSize: 20),
+                                  ),
+                                )
+                            ),
+                          ),
+
+                          SizedBox(height: 20),
+
+
+                          FadeAnimation(2,Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                  border: Border(bottom: BorderSide(color: Colors.grey[100]))
+                              ),
+                              child: Align(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  "About Us",style: TextStyle(color: Colors.grey[400],fontSize: 20),
+                                ),
+                              )
+                          ),
+                          ),
+
+                          SizedBox(height: 20),
+
+
+                          FadeAnimation(2,Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                  border: Border(bottom: BorderSide(color: Colors.grey[100]))
+                              ),
+                              child: Align(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  "About",style: TextStyle(color: Colors.grey[400],fontSize: 20),
+                                ),
+                              )
+                          ),
+                          ),
+
+                          SizedBox(height: 20),
+
+
+                          FadeAnimation(2,Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                  border: Border(bottom: BorderSide(color: Colors.grey[100]))
+                              ),
+                              child: Align(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  "License",style: TextStyle(color: Colors.grey[400],fontSize: 20),
+                                ),
+                              )
+                          ),
+                          ),
+
+                          SizedBox(height: 20),
+
+
+                          FadeAnimation(2,
+                            Container(
+                                padding: EdgeInsets.all(8.0),
+                                decoration: BoxDecoration(
+                                    border: Border(bottom: BorderSide(color: Colors.grey[100]))
+                                ),
+                                child:Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "Contacts",style: TextStyle(color: Colors.grey[400],fontSize: 20),
+                                  ),
+                                )
+                            ),
+                          ),
+
+                ],
+              ),
+                  )),
+              SizedBox(
+                height: 35,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildTextField(
+      String labelText, String placeholder, bool isPasswordTextField) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35.0),
+      child: TextField(
+        //obscureText: isPasswordTextField ? showPassword : false,
+        decoration: InputDecoration(
+            suffixIcon: isPasswordTextField
+                ? IconButton(
+              /* onPressed: () {
+                      setState(() {
+                        showPassword = !showPassword;
+                      });
+                    },*/
+              icon: Icon(
+                Icons.remove_red_eye,
+                color: Colors.grey,
+              ),
+            )
+                : null,
+            contentPadding: EdgeInsets.only(bottom: 3),
+            labelText: labelText,
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            hintText: placeholder,
+            hintStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            )),
       ),
     );
   }
 }
+
+
